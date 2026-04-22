d0fe5650d6dc5a5d961dcb3ddd76de4a15e1c612
root <root@localhost.localdomain>
2026-04-22 01:29:28 +0000

    New folder

---
+++ python_practice/exercise_1.py
@@ -0,0 +1,10 @@
+# Requriement:
+# Find the shortest segment of number from a given number
+# that sum of each digit is greater than target number
+
+import re
+
+number_list = re.findall(r"\d", input("Number: "))
+target_sum = int(input("Target: "))
+
+    
